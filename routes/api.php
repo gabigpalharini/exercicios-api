2f5bdf7c108c19f8bce494ff349272c4f3e58e2d
Your Name <you@example.com>
2023-08-01 10:14:30 -0300

    atualizando ExercicioseteController

--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,7 @@ Route::post('exercicio/quatro',
 
 Route::post('exercicio/cinco', 
 [ExerciciocincoController::class, 'divisivel']);
+
+Route::get('exercicio/sete', 
+[ExercicioSeteController::class, 'exibirNumeros']);
+
